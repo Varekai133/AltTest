f50db66bd7d00ce222014c553b95f0a9fae5a9de
Varekai133 <stebleva.cs@gmail.com>
2021-10-24 20:57:52 +0300

    added all

---
+++ AltTest/AltTest/index.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="AltTest.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        var xhr;
+        var data = new Object();
+        function ProcessImageByUrl() {
+            if (xhr && xhr.readyState != 4) {
+                xhr.abort();
+            }
+            data.url = $("#<%=UploadImageTextBox.ClientID%>")[0].value;
+            data.dropboxValue = $("#<%=OptionDropDownList.ClientID%> option:selected").val();
+            xhr = $.ajax({
+                type: "POST",
+                url: "index.aspx/ProcessImage",
+                data: JSON.stringify(data),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: function (response) {
+                    $("#ProcessedImage").attr("src", "data:image/png;base64," + response.d);
+                }
+            });
+        }
+    </script>
+
+    <link href ="style.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>Image Processing</h1>
+    <hr />
+    <form id="mainform" runat="server">
+        <div>
+            <table class ="center">
+                <tr>
+                    <td>
+                        <asp:TextBox ID="UploadImageTextBox" runat="server"></asp:TextBox>
+                        <asp:DropDownList ID="OptionDropDownList" runat="server">
+                            <asp:ListItem Text="Gray" Value="GrayValue"></asp:ListItem>
+                            <asp:ListItem Text="SwapGandB" Value="SwapGandBValue"></asp:ListItem>
+                        </asp:DropDownList>
+                        <input type="button" id="ProcessImageByUrlButton" onclick = "ProcessImageByUrl()" value="Process""/>
+                    </td>
+                </tr>
+                <tr>
+                    <th align="center"><u>Processed Image</u></th>
+                </tr>
+                <tr>
+                    <td ><img id="ProcessedImage" height ="240px" width ="320px"/></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ AltTest/AltTest/index.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.IO;
+using System.Web.Services;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Net;
+using System.Threading;
+
+namespace AltTest
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        
+        [WebMethod()]
+        public static string ProcessImage(string url, string dropboxValue)
+        {
+            Bitmap mainBitmap;
+            byte[] byteArrayFromImageUrl;
+            using (WebClient client = new WebClient())
+            {
+                byteArrayFromImageUrl = client.DownloadData(url);
+            }
+            using (MemoryStream ms = new MemoryStream(byteArrayFromImageUrl))
+            {
+                mainBitmap = new Bitmap(ms);
+            }
+            
+            if (dropboxValue == "GrayValue")
+                ToGrayProcessImage(mainBitmap);
+            if (dropboxValue == "SwapGandBValue")
+                SwapGandBProcessImage(mainBitmap);
+
+            byte[] byteArrayOfResultImage;
+            using (MemoryStream ms2 = new MemoryStream())
+            {
+                mainBitmap.Save(ms2, System.Drawing.Imaging.ImageFormat.Png);
+                byteArrayOfResultImage = ms2.ToArray();
+            }
+            Thread.Sleep(5000);
+            return Convert.ToBase64String(byteArrayOfResultImage);
+        }
+
+        private static void ToGrayProcessImage(Bitmap bitmap)
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    byte grayColor = (byte)(.299 * pixelColor.R + .587 * pixelColor.G + .114 * pixelColor.B);
+                    bitmap.SetPixel(x, y, Color.FromArgb(grayColor, grayColor, grayColor));
+                }
+            }
+        }
+
+        private static void SwapGandBProcessImage(Bitmap bitmap)
+        {
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixelColor = bitmap.GetPixel(x, y);
+                    bitmap.SetPixel(x, y, Color.FromArgb(pixelColor.R, pixelColor.B, pixelColor.G));
+                }
+            }
+        }
+    }
+}
